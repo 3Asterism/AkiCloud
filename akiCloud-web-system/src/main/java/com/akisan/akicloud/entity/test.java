8bf3bf9475f6521dc5dbc677f74b67be4d47b586
A3ter1sm <842384412@qq.com>
2023-09-19 10:27:58 +0800

    测试案例完善

--- akiCloud-web-system/src/main/java/com/akisan/akicloud/entity/test.java
+++ akiCloud-web-system/src/main/java/com/akisan/akicloud/entity/test.java
@@ -1,9 +1,13 @@
 package com.akisan.akicloud.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(name = "test", description = "测试用pojo类")
 public class test {
+    @Schema(name = "id", description = "测试id")
     private int id;
+    @Schema(name = "name",description = "测试字段")
     private String name;
 }
