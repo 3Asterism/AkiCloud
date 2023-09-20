5994d510758c7a947670fb22396cd842ca327252
A3ter1sm <842384412@qq.com>
2023-09-20 14:34:47 +0800

    添加热配置加载逻辑

--- akiCloud-web-system/src/main/java/com/akisan/akicloud/service/Impl/getTestAutoConfigImpl.java
+++ akiCloud-web-system/src/main/java/com/akisan/akicloud/service/Impl/getTestAutoConfigImpl.java
@@ -2,9 +2,11 @@ package com.akisan.akicloud.service.Impl;
 
 import com.akisan.akicloud.service.getTestAutoConfig;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 @Service
+@RefreshScope
 public class getTestAutoConfigImpl implements getTestAutoConfig {
     @Value("${pattern.dateformat}")
     private String dateformat;
